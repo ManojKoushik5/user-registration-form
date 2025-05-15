14a7078c942515341164a3f7417ed39dfc81fcde
MANOJKOUSHIK <133034803+ManojKoushik5@users.noreply.github.com>
2025-05-15 20:06:29 +0530

    Delete User_Registration_Form_Assignment3 directory

--- User_Registration_Form_Assignment3/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('registrationForm').addEventListener('submit', function(e) {
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
-    if (password !== confirmPassword) {
-        alert('Passwords do not match!');
-        e.preventDefault();
-    }
-});
